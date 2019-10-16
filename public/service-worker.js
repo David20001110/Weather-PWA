1db27c85970f6e5514e0d78cec744822c966b3f3
TrentYetzer <trentyetzer0@gmail.com>
2019-10-16 15:08:44 -0500

    Fixed image reference

--- public/service-worker.js
+++ public/service-worker.js
@@ -24,27 +24,27 @@ const DATA_CACHE_NAME = "data-cache-v1";
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
   "./",
-  "./index.html",
+  "index.html",
   "./scripts/app.js",
   "./scripts/install.js",
   "./scripts/luxon-1.11.4.js",
   "./styles/inline.css",
-  "./images/add.svg",
-  "./images/clear-day.svg",
-  "./images/clear-night.svg",
-  "./images/cloudy.svg",
-  "./images/fog.svg",
-  "./images/hail.svg",
-  "./images/install.svg",
-  "./images/partly-cloudy-day.svg",
-  "./images/partly-cloudy-night.svg",
-  "./images/rain.svg",
-  "./images/refresh.svg",
-  "./images/sleet.svg",
-  "./images/snow.svg",
-  "./images/thunderstorm.svg",
-  "./images/tornado.svg",
-  "./images/wind.svg"
+  "/images/add.svg",
+  "/images/clear-day.svg",
+  "/images/clear-night.svg",
+  "/images/cloudy.svg",
+  "/images/fog.svg",
+  "/images/hail.svg",
+  "/images/install.svg",
+  "/images/partly-cloudy-day.svg",
+  "/images/partly-cloudy-night.svg",
+  "/images/rain.svg",
+  "/images/refresh.svg",
+  "/images/sleet.svg",
+  "/images/snow.svg",
+  "/images/thunderstorm.svg",
+  "/images/tornado.svg",
+  "/images/wind.svg"
 ];
 
 self.addEventListener("install", evt => {
@@ -95,8 +95,7 @@ self.addEventListener("activate", evt => {
 
 self.addEventListener("fetch", evt => {
   console.log("[ServiceWorker] Fetch", evt.request.url);
-  // CODELAB: Add fetch event handler here.
-  // CODELAB: Add fetch event handler here.
+    // CODELAB: Add fetch event handler here.
   if (evt.request.url.includes("/forecast/")) {
     console.log("[Service Worker] Fetch (data)", evt.request.url);
     evt.respondWith(
--- server.js
+++ server.js
@@ -184,7 +184,7 @@ function startServer() {
   app.get('/forecast', getForecast);
 
   // Handle requests for static files
-  app.use(express.static('public'));
+  app.use(express.static('./public'));
 
   // Start the server
   return app.listen('8000', () => {
